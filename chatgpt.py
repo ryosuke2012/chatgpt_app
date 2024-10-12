4636072db6dc42431cae26b3441b1cd6641d2085
ryosuke2012 <117160982+ryosuke2012@users.noreply.github.com>
2024-10-12 10:32:32 +0900

    AIの応答から必要な情報の抽出

--- chatgpt.py
+++ chatgpt.py
@@ -4,15 +4,16 @@ from openai import OpenAI
 
 load_dotenv()  # .envファイルを読み込み
 client = OpenAI(api_key=os.getenv('API_KEY'))  #環境変数を取得
-
 # はじめの説明を表示
 print("AIアシスタントとチャットを始めます。チャットを終了させる場合はexit()と入力してください。")
 
 # チャットのログを保存するリスト
 chat_log: list[dict] = []
 
+# todo AIに与える役割を入力
+
 while True:
-    prompt = input("プロンプトを入力してください：")
+    prompt = input("あなた：")
     if prompt == "exit()":
         break
 
@@ -23,4 +24,11 @@ while True:
     response = client.chat.completions.create(model="gpt-4o-mini", messages=chat_log)
 
     # AIの応答を表示
-    print("AI:", response.model_dump_json(indent=2))
+    content = response.choices[0].message.content
+
+    # ロールを取得
+    role = response.choices[0].message.role
+
+    print(f"AI: {content}")
+
+    chat_log.append({"role":role, "content":content})
