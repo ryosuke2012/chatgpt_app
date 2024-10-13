8ffd72823b576c8534dbbe8fe4a25b31cb06ae9a
ryosuke2012 <117160982+ryosuke2012@users.noreply.github.com>
2024-10-13 12:36:04 +0900

    チャンクで応答する部分の関数化とコードの整理整頓

--- chatgpt.py
+++ chatgpt.py
@@ -5,22 +5,24 @@ from httpx import stream
 from openai import OpenAI
 
 def chat_runner():
-    """"チャットする関数"""
-
-    # はじめの説明を表示
-    print("AIアシスタントとチャットを始めます。チャットを終了させる場合はexit()と入力してください。\n")
+    """"チャットを開始"""
 
     # チャットのログを保存するリスト
     chat_log: list[dict] = []
 
-    # AIに与える役割を入力
+    # はじめの説明を表示
+    print("AIアシスタントとチャットを始めます。チャットを終了させる場合はexit()と入力してください。\n")
+
+    # AIアシスタントに与える役割を入力
     system_role = input("AIアシスタントに与える役割がある場合は入力してください。\n"
                         "ない場合はそのままEnterキーを押してください。：")
+
+    # 役割がある場合はチャットログに追加
     if system_role:
         chat_log.append({"role":"system", "content":system_role})
 
     while True:
-        prompt = input("あなた：")
+        prompt = input("\nあなた：")
         if prompt == "exit()":
             break
 
@@ -28,26 +30,42 @@ def chat_runner():
         chat_log.append({"role":"user", "content":prompt})
 
         # AIの応答を取得
-        response = client.chat.completions.create(model="gpt-4o-mini", messages=chat_log, stream=True)
+        # todoモデルを選択できる関数を実装
+        response = client.chat.completions.create(model="gpt-4o-mini",
+                                                  messages=chat_log,
+                                                  stream=True)
 
-        print("AIアシスタント： ", end="")
-        content = []
-        role = ""
-        for chunk in response:
-            delta = chunk.choices[0].delta
-            content_chunk = delta.content if delta.content is not None else ""
-            role_chunk = delta.role
-            if role_chunk:
-                role = role_chunk
-            if content_chunk:
-                content.append(content_chunk)
-            print(content_chunk, end="")
-        else:
-            print()
-            content = "".join(content)
+        # ストリーミングでAIの応答を取得
+        role, content = stream_and_concatenate_response(response)
 
-        chat_log.append({"role":role, "content":content})
+        # チャットログにAIの応答を追加
+        chat_log.append({"role": role, "content": content})
+
+def stream_and_concatenate_response(response) -> tuple[str, str]:
+    """
+    AIの応答をストリーミングで取得したものをチャンクで表示し、結合して返す
+    :param response: OpenAI.chat.completions.create()の戻り値
+    :return: AIの応答の文字列、AIの応答の役割
+    """
+
+    print("\nAIアシスタント： ", end="")
+    content_list: list[str] = []
+    role = ""
+    for chunk in response:
+        chunk_delta = chunk.choices[0].delta
+        content_chunk = chunk_delta.content if chunk_delta.content is not None else ""
+        role_chunk = chunk_delta.role
+        if role_chunk:
+            role = role_chunk
+        content_list.append(content_chunk)
+        print(content_chunk, end="")
+    else:
+        print()
+        content = "".join(content_list)
+
+    return role, content
 
 load_dotenv()  # .envファイルを読み込み
 client = OpenAI(api_key=os.getenv('API_KEY'))
+
 chat_runner()
