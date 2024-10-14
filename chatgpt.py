546b98f21f9088682bc2f1cf03d1be78b96ce88e
ryosuke2012 <117160982+ryosuke2012@users.noreply.github.com>
2024-10-14 19:37:34 +0900

    GPTモデル一覧を取得する関数の実装

--- chatgpt.py
+++ chatgpt.py
@@ -65,7 +65,31 @@ def stream_and_concatenate_response(response) -> tuple[str, str]:
 
     return role, content
 
+def fetch_gpt_model_list() -> list[str]:
+    """
+    GPTモデルの一覧を取得
+    :return: GPTモデルの一覧
+    """
+
+    # モデルの一覧の取得
+    all_model_list = client.models.list()
+
+    # GPTモデルのみ抽出する
+    gpt_model_list = []
+    for model in all_model_list:
+        if "gpt" in model.id:
+            gpt_model_list.append(model.id)
+
+    # モデル名でソート
+    gpt_model_list.sort()
+
+    return gpt_model_list
+
 load_dotenv()  # .envファイルを読み込み
 client = OpenAI(api_key=os.getenv('API_KEY'))
 
-chat_runner()
+# chat_runner()
+
+model_list = fetch_gpt_model_list()
+for model in model_list:
+    print(model)
