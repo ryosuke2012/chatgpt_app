f7fb7ae5c3ef111821d5981610bc7c6cb0edc146
ryosuke2012 <117160982+ryosuke2012@users.noreply.github.com>
2024-10-17 10:12:06 +0900

    ワークシートを返す関数と新規作成したシートをアクティブにして一番左のタブに移動させる実装

--- output_excel.py
+++ output_excel.py
@@ -46,9 +46,30 @@ def load_or_create_workbook() -> tuple[openpyxl.workbook, bool]:
         wb = openpyxl.Workbook()
         return wb, True
 
+def create_worksheet(title: str, wb: openpyxl.Workbook, is_new: bool):
+    """
+    シートを作成してシート名に使えない文字を除去したうえでシート名を変更して返す
+    :param title: シート名(プロンプトの要約)
+    :param wb: 対象になるワークブックオブジェクト
+    :param is_new: ワークブックが新規作成されたかどうかのフラグ
+    :return: ワークシートオブジェクト
+    """
+
+    if is_new:
+        # アクティブシート(Sheet)を取得
+        ws = wb.active
+        ws.title = title
+    else:
+        # シートを追加
+        ws = wb.create_sheet(title=title)
+        wb.move_sheet(ws, offset=-len(wb.worksheets)+1)
+        wb.active = ws
+    return ws
+
 is_output = is_output_open_excel()
 # print(is_output)
 
 workbook, is_created = load_or_create_workbook()
+worksheet = create_worksheet("test", workbook, is_created)
 workbook.save(excel_path)
-print(workbook.sheetnames)
+print(worksheet.title)
