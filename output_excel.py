fde07e3d8f153875dd1afc8d301fe0898f874a69
ryosuke2012 <117160982+ryosuke2012@users.noreply.github.com>
2024-10-17 09:00:25 +0900

    Excelファイルを新規作成して返す関数の実装

--- output_excel.py
+++ output_excel.py
@@ -2,6 +2,8 @@ import os
 import subprocess
 from pathlib import Path
 
+import openpyxl
+
 base_dir = Path(__file__).parent
 excel_path = base_dir / 'chat_log.xlsx'
 
@@ -28,5 +30,23 @@ def is_output_open_excel() -> bool:
             return bool(result.stdout)
         return False
 
+def load_or_create_workbook():
+    """
+    Excelファイルを読み込むか、存在しない場合は作成する
+    :return: ワークブックオブジェクトと、ファイルが作成されたかどうかのフラグ
+    """
+
+    # ファイルの存在確認
+    if excel_path.exists():
+        # ファイルを読み込んで返す
+        pass
+    else:
+        # ファイルを作成して返す
+        wb = openpyxl.Workbook()
+        return wb
+
 is_output = is_output_open_excel()
 print(is_output)
+
+workbook = load_or_create_workbook()
+workbook.save(excel_path)
