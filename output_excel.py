4a9cfa50c0af7b4ccecfa3b7cc52ec86833ac526
ryosuke2012 <117160982+ryosuke2012@users.noreply.github.com>
2024-10-18 10:35:34 +0900

    シートのヘッダーセルに値出力と書式設定する関数の実装

--- output_excel.py
+++ output_excel.py
@@ -1,6 +1,10 @@
 import os
 import subprocess
+from datetime import datetime
 from pathlib import Path
+
+import openpyxl
+from openpyxl.styles import Font, PatternFill
 
 import openpyxl
 
@@ -81,10 +85,44 @@ def trim_invalid_chars(title: str) -> str:
         title = title.replace(char,"")
     return title
 
+def header_formatting(ws):
+    """
+    出力対象のワークシートにヘッダーを設定する
+    :param ws: 出力対処のワークシート
+    """
+
+    # A1セルに出力時の日時を書き込みフォントを設定
+    datetime_cell = ws["A1"]
+    datetime_cell.value = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+    datetime_cell.font = Font(name="メイリオ")
+
+    # A2セルに「ロール」B2セルに「発言内容」と書き込み、フォントとセルの色を設定
+    role_header_cell = ws["A2"]
+    content_header_cell = ws["B2"]
+
+    # セルに値を設定
+    role_header_cell.value = "ロール"
+    content_header_cell.value = "発言内容"
+
+    # フォントを設定
+    header_font_style = Font(name="メイリオ", bold=True, color="FFFFFF")
+    role_header_cell.font = header_font_style
+    content_header_cell.font = header_font_style
+
+    # セルの色を設定
+    header_color = PatternFill(fill_type="solid", fgColor="156B31")
+    role_header_cell.fill = header_color
+    content_header_cell.fill = header_color
+
+    # セルの幅を調整
+    ws.column_dimensions["A"].width = 22
+    ws.column_dimensions["B"].width = 168
+
 is_output = is_output_open_excel()
 # print(is_output)
 
 workbook, is_created = load_or_create_workbook()
 worksheet = create_worksheet("test/\\?[]", workbook, is_created)
+header_formatting(worksheet)
 workbook.save(excel_path)
 print(worksheet.title)
